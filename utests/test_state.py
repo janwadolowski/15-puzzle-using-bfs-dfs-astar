5f259eae58bfb40b4a29db867abe063b4c614f1e
Jan Wadolowski <janwadolowski@gmail.com>
2021-11-25 03:35:28 +0100

    Add mocks for _move function when testing up, down, left and right

--- utests/test_state.py
+++ utests/test_state.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+from pytest_mock import MockerFixture
 
 from memory.State import State
 
@@ -55,17 +56,17 @@ class TestState:
         assert type(test_state) is np.ndarray
         assert test_state.shape == (4, 4)
 
-    def test_get_state_shape(self, create_state):
+    def test_get_state_shape(self, create_state: State):
         test_state_shape = create_state.get_state_shape()
         assert type(test_state_shape) is tuple
         assert test_state_shape == (4, 4)
 
-    def test__find_zero(self, create_state):
+    def test__find_zero(self, create_state: State):
         test_state_index = create_state._find_zero()
         assert type(test_state_index) is tuple
         assert test_state_index == (2, 1)
 
-    def test__swap_values(self, create_state):
+    def test__swap_values(self, create_state: State):
         # [[1, 2, 3, 4],           [[1, 2, 3, 4],
         #  [5, 6, 7, 8],      =>    [5, 6, 7, 8],
         #  [9, 0, 11, 12],          [9, 11, 0, 12],
@@ -76,53 +77,106 @@ class TestState:
             == np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 11, 0, 12], [13, 14, 15, 10]])
         ).all()
 
-    # TODO: mock _move()
-    def test_up(self, create_state):
+    def test_up(self, create_state: State, mocker: MockerFixture):
         # [[1, 2, 3, 4],           [[1, 2, 3, 4],
         #  [5, 6, 7, 8],      =>    [5, 0, 7, 8],
         #  [9, 0, 11, 12],          [9, 6, 11, 12],
         #  [13, 14, 15, 10]]        [13, 14, 15, 10]]
-        state_after_move = create_state.up()
-        assert (
-            state_after_move.state
-            == np.array([[1, 2, 3, 4], [5, 0, 7, 8], [9, 6, 11, 12], [13, 14, 15, 10]])
-        ).all()
 
-    # TODO: mock _move()
-    def test_down(self, create_state):
+        # Patch internal function
+        new_state_mocked = State(
+            state=np.array(
+                [[1, 2, 3, 4], [5, 0, 7, 8], [9, 6, 11, 12], [13, 14, 15, 10]]
+            ),
+            parent=create_state,
+            preceding_operator="up",
+        )
+        move_patch = mocker.patch(
+            target="memory.State.State._move", return_value=new_state_mocked
+        )
+
+        new_state = create_state.up()
+
+        # Assertions
+        move_patch.assert_called_once_with("up")
+        assert new_state == new_state_mocked
+
+    def test_down(self, create_state: State, mocker: MockerFixture):
         # [[1, 2, 3, 4],           [[1, 2, 3, 4],
         #  [5, 6, 7, 8],      =>    [5, 6, 7, 8],
         #  [9, 0, 11, 12],          [9, 14, 11, 12],
         #  [13, 14, 15, 10]]        [13, 0, 15, 10]]
-        state_after_move = create_state.down()
-        assert (
-            state_after_move.state
-            == np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 14, 11, 12], [13, 0, 15, 10]])
-        ).all()
 
-    # TODO: mock _move()
-    def test_left(self, create_state):
+        # Patch internal function
+        new_state_mocked = State(
+            state=np.array(
+                [[1, 2, 3, 4], [5, 6, 7, 8], [9, 14, 11, 12], [13, 0, 15, 10]]
+            ),
+            parent=create_state,
+            preceding_operator="down",
+        )
+        move_patch = mocker.patch(
+            target="memory.State.State._move", return_value=new_state_mocked
+        )
+
+        new_state = create_state.down()
+
+        # Assertions
+        move_patch.assert_called_once_with("down")
+        assert new_state == new_state_mocked
+
+    def test_left(self, create_state: State, mocker: MockerFixture):
         # [[1, 2, 3, 4],           [[1, 2, 3, 4],
         #  [5, 6, 7, 8],      =>    [5, 6, 7, 8],
         #  [9, 0, 11, 12],          [0, 9, 11, 12],
         #  [13, 14, 15, 10]]        [13, 14, 15, 10]]
-        state_after_move = create_state.left()
-        assert (
-            state_after_move.state
-            == np.array([[1, 2, 3, 4], [5, 6, 7, 8], [0, 9, 11, 12], [13, 14, 15, 10]])
-        ).all()
 
-    # TODO: mock _move()
-    def test_right(self, create_state):
+        # Patch internal function
+        new_state_mocked = State(
+            state=np.array(
+                [[1, 2, 3, 4], [5, 6, 7, 8], [0, 9, 11, 12], [13, 14, 15, 10]]
+            ),
+            parent=create_state,
+            preceding_operator="left",
+        )
+        move_patch = mocker.patch(
+            target="memory.State.State._move", return_value=new_state_mocked
+        )
+
+        new_state = create_state.left()
+
+        # Assertions
+        move_patch.assert_called_once_with("left")
+        assert new_state == new_state_mocked
+
+    def test_right(self, create_state: State, mocker: MockerFixture):
         # [[1, 2, 3, 4],           [[1, 2, 3, 4],
         #  [5, 6, 7, 8],      =>    [5, 6, 7, 8],
         #  [9, 0, 11, 12],          [9, 11, 0, 12],
         #  [13, 14, 15, 10]]        [13, 14, 15, 10]]
-        state_after_move = create_state.right()
-        assert (
-            state_after_move.state
-            == np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 11, 0, 12], [13, 14, 15, 10]])
-        ).all()
 
-    def test__check_legal_move(self, state_top_right_corner, state_bottom_left_corner):
+        # Patch internal function
+        new_state_mocked = State(
+            state=np.array(
+                [[1, 2, 3, 4], [5, 6, 7, 8], [9, 11, 0, 12], [13, 14, 15, 10]]
+            ),
+            parent=create_state,
+            preceding_operator="right",
+        )
+        move_patch = mocker.patch(
+            target="memory.State.State._move", return_value=new_state_mocked
+        )
+
+        new_state = create_state.right()
+
+        # Assertions
+        move_patch.assert_called_once_with("right")
+        assert new_state == new_state_mocked
+
+    def test__check_legal_move(
+        self, state_top_right_corner: State, state_bottom_left_corner: State
+    ):
+        pass
+
+    def test__move(self):
         pass
