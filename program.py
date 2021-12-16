6788d5b17370da7894c1e4c37c29481b648f0858
Jan Wadolowski <janwadolowski@gmail.com>
2021-12-16 23:27:00 +0100

    Formatting with black

--- program.py
+++ program.py
@@ -13,10 +13,20 @@ def main() -> None:
     # Example: python program.py bfs RDUL 4x4_01_0001.txt 4x4_01_0001_bfs_rdul_sol.txt 4x4_01_0001_bfs_rdul_stats.txt
     parser = argparse.ArgumentParser()
     parser.add_argument("Strategy", type=str, help="Algorithm [bfs, dfs, astr]")
-    parser.add_argument("Strategy_param", type=str, help="Algorithm [bfs&dfs: permutations of [L,R,U,D]; astr: hamm/manh]")
+    parser.add_argument(
+        "Strategy_param",
+        type=str,
+        help="Algorithm [bfs&dfs: permutations of [L,R,U,D]; astr: hamm/manh]",
+    )
     parser.add_argument("Input_file", type=str, help="Input puzzle .txt file")
-    parser.add_argument("Output_Solution", type=str, help="Output .txt file name with puzzle solution steps")
-    parser.add_argument("Output_Stats", type=str, help="Output .txt file with algorithm statistics")
+    parser.add_argument(
+        "Output_Solution",
+        type=str,
+        help="Output .txt file name with puzzle solution steps",
+    )
+    parser.add_argument(
+        "Output_Stats", type=str, help="Output .txt file with algorithm statistics"
+    )
     args = parser.parse_args()
 
     # Output files
@@ -26,17 +36,17 @@ def main() -> None:
     # Input file path
     input_file_path = os.path.realpath("./" + args.Input_file)
 
-    if args.Strategy == 'bfs':
+    if args.Strategy == "bfs":
         print("Start BFS")
         bfs = BFS
         solve_puzzle(bfs, input_file_path, solution_file, stats_file)
 
-    elif args.Strategy == 'dfs':
+    elif args.Strategy == "dfs":
         print("Start DFS")
         dfs = DFS
         solve_puzzle(dfs, input_file_path, solution_file, stats_file)
 
-    elif args.Strategy == 'astr':
+    elif args.Strategy == "astr":
         print("Start A*")
         astr = AStar
         solve_puzzle(astr, input_file_path, solution_file, stats_file)
@@ -67,13 +77,26 @@ def write_to_solution_file(moves: str, n_moves: int, sol_file) -> None:
         sol_file.write(str(n_moves) + "\n" + moves)
 
 
-def write_to_stats_file(n_moves: int, frontier: int, explored: int, recursion: int, time_elapsed: float,
-                        stats_file) -> None:
-    stats_file.write(str(n_moves) + "\n" +
-                     str(frontier) + "\n" +
-                     str(explored) + "\n" +
-                     str(recursion) + "\n" +
-                     str(format(time_elapsed, '.3f')) + "\n")
+def write_to_stats_file(
+    n_moves: int,
+    frontier: int,
+    explored: int,
+    recursion: int,
+    time_elapsed: float,
+    stats_file,
+) -> None:
+    stats_file.write(
+        str(n_moves)
+        + "\n"
+        + str(frontier)
+        + "\n"
+        + str(explored)
+        + "\n"
+        + str(recursion)
+        + "\n"
+        + str(format(time_elapsed, ".3f"))
+        + "\n"
+    )
 
 
 def solve_puzzle(algorithm, input_file_path, output_solution, output_stats):
@@ -89,7 +112,14 @@ def solve_puzzle(algorithm, input_file_path, output_solution, output_stats):
     # TODO Change path from list to string
     write_to_solution_file(moves, len(moves), output_solution)
     # TODO Uzupełnic parametr głębokość rekursji
-    write_to_stats_file(len(moves), len(algorithm.frontier), len(algorithm.closed_list), 3, time_in_ms, output_stats)
+    write_to_stats_file(
+        len(moves),
+        len(algorithm.frontier),
+        len(algorithm.closed_list),
+        3,
+        time_in_ms,
+        output_stats,
+    )
 
 
 if __name__ == "__main__":
