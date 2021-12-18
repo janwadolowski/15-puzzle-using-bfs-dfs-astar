80a33280075e5c99b3994f7ff75d0b73b5ed4869
bkubiak90 <kubiakbartek90@gmail.com>
2021-12-18 12:29:53 +0100

    DFS: added visited states counter and max depth check

--- program.py
+++ program.py
@@ -95,7 +95,7 @@ def solve_puzzle(algorithm, input_file_path, output_solution, output_stats):
     end_time = datetime.datetime.now()
     # Time difference between markers in milliseconds
     time_in_ms = (end_time - start_time).total_seconds() * 1000.0
-    
+
     write_to_solution_file(moves, output_solution)
     # TODO Uzupełnic parametr głębokość rekursji
     n_moves = -1
@@ -104,9 +104,9 @@ def solve_puzzle(algorithm, input_file_path, output_solution, output_stats):
 
     write_to_stats_file(
         n_moves,
-        algorithm.frontier.qsize(),
+        algorithm.visited_states,
         len(algorithm.closed_list),
-        algorithm.depth,
+        algorithm.max_depth,
         time_in_ms,
         output_stats,
     )
